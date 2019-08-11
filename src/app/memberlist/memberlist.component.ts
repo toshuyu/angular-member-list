0faa77e1a1095afba0a11c1b1ca9eb8d34a67844
toshuyu <toshuyu7@gmail.com>
2019-08-11 09:34:28 +0800

    remove unused Mat

--- src/app/memberlist/memberlist.component.ts
+++ src/app/memberlist/memberlist.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, ViewChild, Inject } from '@angular/core';
 import { MatTable } from '@angular/material/table';
 import { MemberlistService } from './memberlist.service';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { FormdialogComponent } from '../formdialog/formdialog.component';
-import { Member } from '../member-model';
 
 @Component({
   selector: 'app-memberlist',
@@ -32,7 +31,7 @@ export class MemberlistComponent implements OnInit {
     this.memlist.renderRows();
   }
 
-  openDialog(id): void {
+  onEdit(id): void {
     const dialogRef = this.dialog.open(FormdialogComponent, {
       width: '400px',
       data: {...this.memberService.getSingleMember(id),
